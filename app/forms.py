0c621852ba358dd09e34e71a2d4b6d401d0f985d
yural64 <yural64@gmail.com>
2025-03-28 23:29:33 +0300

    edit forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
-
 from app.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Регистрация')
+
 
 def validate_username(self, username):
     user = User.query.filter_by(username=username.data).first()
