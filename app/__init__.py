93e7fb0308805996f94bfac65199c77bd38657ef
yural64 <yural64@gmail.com>
2025-03-29 00:08:57 +0300

    edit init.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from app import routes, models
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -10,6 +11,4 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clicker.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login.view = 'login'
-
-from app import routes, models
+login_manager.login_view = 'login'
