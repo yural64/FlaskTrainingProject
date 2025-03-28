01e2ab56e4468dd015fc84de9112d436f03881c1
yural64 <yural64@gmail.com>
2025-03-28 22:57:15 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 from app import login_manager
 from flask_login import UserMixin
 
+
 class User(db.model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
@@ -9,8 +10,9 @@ class User(db.model, UserMixin):
     clicks = db.Column(db.Integer, defaults=0)
 
     def __repr__(self):
-        return f'User {self.username}' - clicks: {self.clicks}'
+        return f'User {self.username} - clicks: {self.clicks}'
 
-@login_manager.user_loader # Этот декоратор связывает функцию с flask_login, чтобы загружать пользователя по id
+
+@login_manager.user_loader  # Этот декоратор связывает функцию с flask_login, чтобы загружать пользователя по id
 def load_user(user_id):
     return User.query.get(int(user_id))
